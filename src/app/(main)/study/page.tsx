3889620c76191ebd3a2a2af43796225f176a0a1f
sunny-son <thstkd92@gmail.com>
2026-02-08 19:29:28 +0900

    feat : 카테고리 필터 추가

--- src/app/(main)/study/page.tsx
+++ src/app/(main)/study/page.tsx
@@ -1,10 +1,12 @@
 import { CardList } from '@/features/study/components/CardList';
+import { CategoryFilter } from '@/features/study/ui/CategoryFilter';
 
 export default async function Study() {
   return (
     <div className="flex flex-col justify-between items-start h-[758px] px-10 mt-9">
-      <div className="flex flex-col gap-5">
+      <div className="flex flex-col w-full gap-[30px]">
         <h1 className="text-gray-900 text-[32px] font-bold">학습 콘텐츠 선택</h1>
+        <CategoryFilter />
       </div>
       <CardList />
     </div>
